7e6cf0bff3bb94816a698b812e29f0dca831a4f5
Tom Howard <thomaswhoward@gmail.com>
2021-04-09 13:39:31 -0400

    c1-1-1 formatted file

--- chapter-1/1-1.cpp
+++ chapter-1/1-1.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 int main()
 {
-    unordered_set <char> uSet;
+    unordered_set<char> uSet;
     string str;
     cin >> str;
-    for(int i = 0; i < str.length(); i++) {
-        if (uSet.find(str[i]) != uSet.end()) {
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (uSet.find(str[i]) != uSet.end())
+        {
             cout << "Duplicate found";
             return 0;
         }
         uSet.insert(str[i]);
-    } 
+    }
     cout << str;
     return 0;
 }
